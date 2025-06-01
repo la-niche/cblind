dd8e67f5a878c82a8f5d77237ebe6606e5e36199
Clément Robert <cr52@protonmail.com>
2025-06-01 18:54:27 +0200

    BLD: switch build backend to flit-core

--- cblind/__init__.py
+++ cblind/__init__.py
@@ -1,4 +1,3 @@
-from .__version__ import __version__
 from .cblind import Colorplots, cbmap, mapping, reversed_cmap, test_cblind, test_contrast, test_huescale, test_solstice, test_bird, test_pregunta, test_extreme_rainbow, test_rainbow, test_rbscale, test_monocolor, test_mapping
 
 from .cblind import _register_to_mpl, PALETTES
@@ -7,3 +6,10 @@ for name in PALETTES:
 
 del _register_to_mpl
 del PALETTES
+
+
+def __getattr__(item):
+    if item == "__version__":
+        from importlib.metadata import version
+        return version("cblind")
+    raise AttributeError(f"module '{__name__}' has no attribute '{item}'")
